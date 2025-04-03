fe1960e2011388d87d1dd5757efba4df4525048c
b-vic <b_vickery@hotmail.com>
2025-04-03 12:18:19 -0400

    Eliminate inner class for easier remote execution

--- PortChecker.java
+++ PortChecker.java
@@ -4,18 +4,23 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.AbstractMap.SimpleEntry;
 
 public class PortChecker {
 
     private static final Pattern VALID_START_OF_LINE = Pattern.compile("^[a-z0-9]", Pattern.CASE_INSENSITIVE);
     private static final Pattern SERVER_PORT_DELIMITERS = Pattern.compile("[ ,\t:]+");
+    private final List<SimpleEntry<String, Integer>> destinationAddresses;
+    private final long startTime;
 
-    private List<DestinationAddress> destinationAddresses = new ArrayList<>();
+    public PortChecker(String[] args) {
+        this.destinationAddresses = processInputArgs(args);
+        this.startTime = System.currentTimeMillis();
+    }
 
     /**
      * Usage: java PortChecker 127.0.0.1 8080 OR java PortChecker ./filename.txt
@@ -25,24 +30,19 @@ public class PortChecker {
         portChecker.connectToAddresses();
     }
 
-    public PortChecker(String[] args) {
-        this.destinationAddresses = processInputArgs(args);
-    }
-
-    private List<DestinationAddress> processInputArgs(String[] args) {
-        final List<DestinationAddress> addressList;
+    private List<SimpleEntry<String, Integer>> processInputArgs(String[] args) {
+        final List<SimpleEntry<String, Integer>> addressList;
         switch (args.length) {
             case 1:
                 addressList = readAndFilterLines(args[0]);
                 break;
             case 2:
-                addressList = Arrays.asList(new DestinationAddress(args[0], args[1]));
+                addressList = Collections.singletonList(new SimpleEntry<>(args[0], Integer.parseInt(args[1])));
                 break;
             default:
                 throw new RuntimeException("Incorrect usage. Arguments either: hostnameOrIpAddress port OR ./filename.txt");
         }
         return addressList;
-
     }
 
     private void connectToAddresses() {
@@ -52,34 +52,12 @@ public class PortChecker {
                         .map(PortChecker::tryConnect)
                         .mapToInt(result -> result ? 1 : 0)
                         .sum();
-        System.out.printf("==========%nSuccessfully connected to: %d of %d addresses %n", successCount, destinationAddresses.size());
+        System.out.printf("==========%nSuccessfully connected to: %d of %d addresses in %f seconds %n",
+                successCount, destinationAddresses.size(), (System.currentTimeMillis() - startTime) / 1000.0);
     }
 
-    private List<DestinationAddress> readAndFilterLines(String inputFile) {
-        final Path pathToFile = getFile(inputFile);
-        final List<DestinationAddress> addressLines;
-        try {
-            addressLines = Files.readAllLines(pathToFile).stream()
-                    .filter(VALID_START_OF_LINE.asPredicate())
-                    .map(DestinationAddress::new)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            System.err.printf("Aborting due to : %s%n", e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return addressLines;
-    }
-
-    private Path getFile(String inputFile) {
-        final Path pathToFile = Paths.get(inputFile);
-        if (!Files.exists(pathToFile)) {
-            throw new RuntimeException(String.format("There is a problem accessing the input file: %s", inputFile));
-        }
-        return pathToFile;
-    }
-
-    private static boolean tryConnect(DestinationAddress destinationAddress) {
-        try (Socket socket = new Socket(destinationAddress.getHost(), destinationAddress.getPort());
+    private static boolean tryConnect(SimpleEntry<String, Integer> destinationAddress) {
+        try (Socket socket = new Socket(destinationAddress.getKey(), destinationAddress.getValue());
              OutputStream outputStream = socket.getOutputStream()) {
             outputStream.flush();
             System.out.printf("Success connecting to %s%n", destinationAddress);
@@ -90,34 +68,28 @@ public class PortChecker {
         }
     }
 
-    static class DestinationAddress {
-
-        private final String host;
-        private final int port;
-
-        public DestinationAddress(String host, String port) {
-            this.host = host;
-            this.port = Integer.parseInt(port);
+    private List<SimpleEntry<String, Integer>> readAndFilterLines(String inputFile) {
+        final Path pathToFile = getFile(inputFile);
+        List<SimpleEntry<String, Integer>> destinationAddressLines;
+        try {
+            destinationAddressLines = Files.readAllLines(pathToFile).stream()
+                    .filter(VALID_START_OF_LINE.asPredicate())
+                    .map(SERVER_PORT_DELIMITERS::split)
+                    .filter(hostPortArray -> hostPortArray.length == 2)
+                    .map(hostPort -> new SimpleEntry<>(hostPort[0], Integer.parseInt(hostPort[1])))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.printf("Aborting due to : %s%n", e.getMessage());
+            throw new RuntimeException(e);
         }
+        return destinationAddressLines;
+    }
 
-        public DestinationAddress(String hostAndPort) {
-            //Allow any of the usual delimiters: space(s), comma, tab or colon:
-            String[] values = SERVER_PORT_DELIMITERS.split(hostAndPort);
-            this.host = values[0];
-            this.port = Integer.parseInt(values[1]);
+    private Path getFile(String inputFile) {
+        final Path pathToFile = Paths.get(inputFile);
+        if (!Files.exists(pathToFile)) {
+            throw new RuntimeException(String.format("There is a problem accessing the input file: %s", inputFile));
         }
-
-        public String getHost() {
-            return host;
-        }
-
-        public int getPort() {
-            return port;
-        }
-
-        @Override
-        public String toString() {
-            return "host=" + this.getHost() + ", port=" + this.getPort();
-        }
+        return pathToFile;
     }
 }
